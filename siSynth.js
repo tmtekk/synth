e9c3bf08dd06d3a2ae48b07939fb04031a920b7a
Tim <owner@Owners-MacBook-Pro.local>
2018-01-22 17:58:39 -0500

    added new oscillators and styling

--- siSynth.js
+++ siSynth.js
@@ -1,19 +1,46 @@
 
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-
-osc.onmousedown = function(){
+function play1(){
 var osc = document.getElementById("osc");
 var oscillator = audioCtx.createOscillator();
 oscillator.type = 'sine';
-oscillator.frequency.value = 200;
+oscillator.frequency.value = 261.6;
 oscillator.connect(audioCtx.destination);
 currentTime = audioCtx.currentTime;
 oscillator.start(audioCtx.currentTime);
-oscillator.stop(currentTime + 2);
+oscillator.stop(currentTime + 1);
 };
 
-// osc.onmouseup = function(){
-//   oscillator.stop();
-//   oscillator.disconnect();
-// };
+function play2(){
+var osc = document.getElementById("osc2");
+var oscillator = audioCtx.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequency.value = 293.7;
+oscillator.connect(audioCtx.destination);
+currentTime = audioCtx.currentTime;
+oscillator.start(audioCtx.currentTime);
+oscillator.stop(currentTime + 0.7);
+};
+
+function play3(){
+var osc = document.getElementById("osc3");
+var oscillator = audioCtx.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequency.value = 293.7;
+oscillator.connect(audioCtx.destination);
+currentTime = audioCtx.currentTime;
+oscillator.start(audioCtx.currentTime);
+oscillator.stop(currentTime + 0.7);
+};
+
+function play3(){
+var osc = document.getElementById("osc3");
+var oscillator = audioCtx.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequency.value = 329.6;
+oscillator.connect(audioCtx.destination);
+currentTime = audioCtx.currentTime;
+oscillator.start(audioCtx.currentTime);
+oscillator.stop(currentTime + 0.7);
+};
