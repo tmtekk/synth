3f92edb827ea047f9a916a8c6440461630eb64c8
Tim <owner@Owners-MacBook-Pro.local>
2018-01-22 18:43:36 -0500

    added new oscillators and styling

--- siSynth.js
+++ siSynth.js
@@ -27,18 +27,62 @@ function play3(){
 var osc = document.getElementById("osc3");
 var oscillator = audioCtx.createOscillator();
 oscillator.type = 'sine';
-oscillator.frequency.value = 293.7;
+oscillator.frequency.value = 329.6;
 oscillator.connect(audioCtx.destination);
 currentTime = audioCtx.currentTime;
 oscillator.start(audioCtx.currentTime);
 oscillator.stop(currentTime + 0.7);
 };
 
-function play3(){
-var osc = document.getElementById("osc3");
+function play4(){
+var osc = document.getElementById("osc4");
 var oscillator = audioCtx.createOscillator();
 oscillator.type = 'sine';
-oscillator.frequency.value = 329.6;
+oscillator.frequency.value = 349.2;
+oscillator.connect(audioCtx.destination);
+currentTime = audioCtx.currentTime;
+oscillator.start(audioCtx.currentTime);
+oscillator.stop(currentTime + 0.7);
+};
+
+function play5(){
+var osc = document.getElementById("osc5");
+var oscillator = audioCtx.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequency.value = 392.0;
+oscillator.connect(audioCtx.destination);
+currentTime = audioCtx.currentTime;
+oscillator.start(audioCtx.currentTime);
+oscillator.stop(currentTime + 0.7);
+};
+
+function play6(){
+var osc = document.getElementById("osc6");
+var oscillator = audioCtx.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequency.value = 440.0;
+oscillator.connect(audioCtx.destination);
+currentTime = audioCtx.currentTime;
+oscillator.start(audioCtx.currentTime);
+oscillator.stop(currentTime + 0.7);
+};
+
+function play7(){
+var osc = document.getElementById("osc7");
+var oscillator = audioCtx.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequency.value = 493.9;
+oscillator.connect(audioCtx.destination);
+currentTime = audioCtx.currentTime;
+oscillator.start(audioCtx.currentTime);
+oscillator.stop(currentTime + 0.7);
+};
+
+function play8(){
+var osc = document.getElementById("osc8");
+var oscillator = audioCtx.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequency.value = 523.3;
 oscillator.connect(audioCtx.destination);
 currentTime = audioCtx.currentTime;
 oscillator.start(audioCtx.currentTime);
